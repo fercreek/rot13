eb5269fe3279156dec5bea453488793fd9c69b16
Fer Contreras <luis_fercast@hotmail.com>
2014-08-10 12:50:07 -0500

    Uso de modulo

--- rot13.py
+++ rot13.py
@@ -1,15 +1,12 @@
 import sys
-def encoding(msg, c):
+def encoding(msg,c):
 	alpha = "abcdefghijklmnopqrstuvwxyz"
-	text = ""
-	for letter in msg:
-		if letter != " ":
-			pos = alpha.find(letter)
-			if pos == -1 :
-				text += ""
-			else:	
-				text += alpha[pos+c] if pos+c<=26 else alpha[pos+c-26]
-		else: 	
-			text+=""	
-	return text
+ 	text = ""
+ 	for letter in msg:
+ 		pos = alpha.find(letter)
+ 		if pos != -1:
+ 			text+= alpha[(pos+13)%26]
+ 		else:
+ 			text+=""
+ 	return text
 print encoding(sys.argv[1], 13)
